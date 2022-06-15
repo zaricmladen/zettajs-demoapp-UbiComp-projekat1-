327472aa72893b387422ec74a012197047b2efdc
zaricmladen <48604690+zaricmladen@users.noreply.github.com>
2022-06-15 18:40:03 +0200

    Folders renamed

--- Dusk2DawnApp/index.js
+++ Dusk2DawnApp/index.js
@@ -1,10 +1,19 @@
 process.EventEmitter = require('events').EventEmitter;
+
 var zetta = require('zetta');
-var DataDevice = require('./data-device.js');
+var LED = require('zetta-led-mock-driver');
+var Photocell = require('zetta-photocell-mock-driver');
+var duskToDawnLight = require('./apps/dusk_to_dawn_light');
+var StateMachineScout = require('./scout.js');
+var StateMachineApp = require('./apps/app.js');
 
 zetta()
-  .name('Data streaming server')
-  .use(DataDevice)
+  .name('DuskTillDawnServer')
+  .use(LED)
+  .use(Photocell)
+  .use(duskToDawnLight)
+  .use(StateMachineScout)
+  .use(StateMachineApp)
   .listen(1337, function(){
-     console.log('Zetta is running at http://127.0.0.1:1337');
+  console.log('Zetta is running at http://127.0.0.1:1337');
 });
